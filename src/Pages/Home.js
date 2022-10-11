2792d447773f4005a284f231101424e8b0ec10e6
Ramazan Aliyev <ramazanal@azerimed.local>
2022-10-11 11:07:20 +0400

    finished

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
 	return (
-		<div className="h-[calc(100vh-20vh)]">
+		<div className="h-[calc(100vh-20vh)] bgImageAzerbaijanFlag">
 			<div className="w-[95%] mx-auto">Home</div>
 		</div>
 	);
